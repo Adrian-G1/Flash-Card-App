ffb9843cf0f5dcd04ef48fa88c8891205f26e513
Adrian Garcia <agarciacarillo@gmail.com>
2023-02-06 20:24:19 -0600

    implemented card flip

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from tkinter import *
+from random import choice
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+# ----------------------------- FLIP CARD ------------------------------ #
+def flip_card(card: dict):
+    canvas.itemconfig(card_image, image=back_card_img)
+    canvas.itemconfig(title_text, text="English")
+    canvas.itemconfig(word_text, text=card["English"])
+
+
 # --------------------------- NEW FLASH CARD --------------------------- #
 data = pandas.read_csv("data/french_words.csv")
-data_dict = data.to_dict()
+to_learn = data.to_dict(orient="records")
+
+
+def new_card():
+    current_card = choice(to_learn)
+    canvas.itemconfig(word_text, text=current_card["French"])
+    canvas.itemconfig(card_image, image=front_card_img)
+    canvas.itemconfig(title_text, text="French")
+    window.after(3000, flip_card, current_card)
+
 
 # ------------------------------ UI SETUP ------------------------------ #
 window = Tk()
@@ -15,9 +33,10 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # Canvas
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 front_card_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=front_card_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"), fill="black")
+back_card_img = PhotoImage(file="images/card_back.png")
+card_image = canvas.create_image(400, 263, image=front_card_img)
+title_text = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"), fill="black")
+word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"), fill="black")
 canvas.grid(column=0, row=0, columnspan=2)
 
 # # Label
@@ -31,11 +50,13 @@ canvas.grid(column=0, row=0, columnspan=2)
 
 # Button
 cross_img = PhotoImage(file="images/wrong.png")
-cross_button = Button(image=cross_img, borderwidth=0)
+cross_button = Button(image=cross_img, borderwidth=0, command=new_card)
 cross_button.grid(column=0, row=1)
 
 check_img = PhotoImage(file="images/right.png")
-check_button = Button(image=check_img, borderwidth=0)
+check_button = Button(image=check_img, borderwidth=0, command=new_card)
 check_button.grid(column=1, row=1)
+
+new_card()
 
 window.mainloop()
